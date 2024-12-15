5ff0e3234011ba96a7f6a579a01280eada14a90a
anthonysackman <60375685+anthonysackman@users.noreply.github.com>
2024-12-14 21:40:03 -0800

    remove database work i am disgusted with

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from sanic import Sanic
 from app.routes import setup_routes
 from config import Config
-from app.database import setup_database, shutdown_database
 import redis.asyncio as redis
 
 
@@ -10,30 +9,9 @@ def create_app():
     app = Sanic("Helper")
     app.config.update(Config.as_dict())
     setup_routes(app)
-
-    # Call methods for setting up database and Redis
-    setup_app_database(app)
     setup_app_redis(app)
 
     return app
-
-
-def setup_app_database(app):
-    """Setup database lifecycle events for the app."""
-
-    @app.before_server_start
-    async def connect_to_db(app, _):
-        """Connect to the database and create tables."""
-        await setup_database(
-            database_url=app.config["DATABASE_URL"],
-            echo=app.config.get("SQLALCHEMY_ECHO", False)
-        )
-        app.ctx.db_sessionmaker = app.ctx.db_sessionmaker
-
-    @app.after_server_stop
-    async def disconnect_from_db(app, _):
-        """Disconnect from the database."""
-        await shutdown_database()
 
 
 def setup_app_redis(app):
